fc141b1713f2f66aaa41e23d0390d71bf2c3f4e0
AladinX0X <121769504+AladinX0X@users.noreply.github.com>
2024-03-20 01:35:45 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
-import {  Router } from '@angular/router';
-import { OnDestroy } from  '@angular/core';
-
+import { Router } from '@angular/router';
+import { OnDestroy } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnDestroy{
+export class AppComponent implements OnDestroy {
   title = 'AppleShop';
-  constructor(private router: Router){}
+  
+  constructor(private router: Router, private translate: TranslateService) {
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
+  
   ngOnDestroy() {
     this.router.navigate([this.router.url]);
-  }
+  }
 }
